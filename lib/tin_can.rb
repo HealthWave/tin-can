9d0b1bc893ca158e37ba7a994c37d81d597f6fb9
Paulo Arruda <paulo@healthwave.co>
2015-08-04 11:35:43 -0400

    moved rightly inside the main file

--- lib/tin_can.rb
+++ lib/tin_can.rb
@@ -6,7 +6,11 @@ require "tin_can/event_controller"
 require "tin_can/event_handler"
 
 module TinCan
-  require 'tin_can/railtie' if defined?(Rails)
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      load 'tasks/tin_can.rake'
+    end
+  end
 
   @@handler = nil
   @@routes = nil
